15911b844edbc8c9e599cf813ef6d810569d5c62
JoaquinCostamagna <jncostamagna@gmail.com>
2022-05-17 18:47:04 -0300

    Comentarios en el clock

--- intro-multipantalla/src/Clock.js
+++ intro-multipantalla/src/Clock.js
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 
 
 function Clock(props){
+    // Creo estado de date
     const [date, setDate] = useState(new Date());
+    // 
     useEffect(() =>{
+        // Función que setea la fecha y hora en el estado del componente
         function tick(){
             setDate(new Date());
         }
-
+        // Timer llama a la función tick() cada un segundo siempre y cuando este activada la bandera de tick
         const timerId = setInterval(()=>{
             if(props.tick) tick();
         }, 1000);
