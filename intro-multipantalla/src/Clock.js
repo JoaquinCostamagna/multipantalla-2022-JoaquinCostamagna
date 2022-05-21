36287bd6096c2416d84130c4bd3b7e758bfd28e6
JoaquinCostamagna <jncostamagna@gmail.com>
2022-05-21 11:40:38 -0300

    Creación de proyecto react-native

--- intro-multipantalla/src/Clock.js
+++ intro-multipantalla/src/Clock.js
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 
 
-function Clock(props){
+function Clock(props) {
     // Creo estado de date
     const [date, setDate] = useState(new Date());
     // 
-    useEffect(() =>{
+    useEffect(() => {
         // Función que setea la fecha y hora en el estado del componente
-        function tick(){
+        function tick() {
             setDate(new Date());
         }
         // Timer llama a la función tick() cada un segundo siempre y cuando este activada la bandera de tick
-        const timerId = setInterval(()=>{
-            if(props.tick) tick();
+        const timerId = setInterval(() => {
+            if (props.tick) tick();
         }, 1000);
-        
-        return () => clearInterval(timerId);
-    },[props.tick])
 
-    return(
+        return () => clearInterval(timerId);
+    }, [props.tick])
+
+    return (
         <h2>{date.toLocaleTimeString()}</h2>
     );
 }
